0e22078f36311454ecd6d59c2639db307a876eeb
Emir Kurtovic <ekurtovic3@etf.unsa.ba>
2020-09-01 01:05:16 +0200

    Valdacija za korisnika

--- src/ba/unsa/etf/rs/MainController.java
+++ src/ba/unsa/etf/rs/MainController.java
@@ -1,13 +1,19 @@
 package ba.unsa.etf.rs;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.ListView;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.YearMonth;
 
 public class MainController {
 
@@ -16,6 +22,12 @@ public class MainController {
     public ListView listViewUsers;
     public ListView listViewClassroom;
     public TextField fldName;
+    public TextField fldSurname;
+    public TextField fldEmail;
+    public TextField fldUsername;
+    public TextField fldJmbg;
+    public DatePicker dpBirthday;
+    public PasswordField fldPassword;
     private TimetableDAO dao;
     private ObservableList<Subject> listSubjects;
     private ObservableList<User> listUsers;
@@ -29,20 +41,142 @@ public class MainController {
     }
     @FXML
     public void initialize() {
-     //   dao.defaultClass();
         listViewSubjects.setItems(dao.getAllSubjects());
         listViewUsers.setItems(dao.getAllUsers());
         listViewClassroom.setItems(dao.getAllClassrooms());
+
         listViewUsers.getSelectionModel().selectedItemProperty().addListener((obs, oldKorisnik, newKorisnik) -> {
             dao.setTrenutniKorisnik((User) newKorisnik);
 
         });
         dao.trenutniKorisnikProperty().addListener((obs, oldKorisnik, newKorisnik) -> {
-                fldName.textProperty().bindBidirectional( newKorisnik.nameProperty() );
+            if (oldKorisnik != null) {
+                fldName.textProperty().unbindBidirectional(oldKorisnik.nameProperty() );
+                fldSurname.textProperty().unbindBidirectional(oldKorisnik.surnameProperty() );
+                fldEmail.textProperty().unbindBidirectional(oldKorisnik.emailProperty() );
+                fldUsername.textProperty().unbindBidirectional(oldKorisnik.usernameProperty() );
+                fldJmbg.textProperty().unbindBidirectional(oldKorisnik.jmbgProperty() );
+             //   fldPassword.textProperty().unbindBidirectional(oldKorisnik.() );
+              //  dpBirthday.textProperty().unbindBidirectional(oldKorisnik.passwordProperty() );
+            }
+            if (newKorisnik == null) {
+                fldName.setText("");
+                fldSurname.setText("");
+                fldEmail.setText("");
+                fldUsername.setText("");
+                fldJmbg.setText("");
+                fldPassword.setText("");
+                dpBirthday.setValue(LocalDate.now());
+            }
+            else {
+                fldName.textProperty().bindBidirectional(newKorisnik.nameProperty());
+                fldSurname.textProperty().bindBidirectional(newKorisnik.surnameProperty() );
+                fldEmail.textProperty().bindBidirectional(newKorisnik.emailProperty() );
+                fldUsername.textProperty().bindBidirectional(newKorisnik.usernameProperty() );
+                fldJmbg.textProperty().bindBidirectional(newKorisnik.jmbgProperty() );
+               // fldPassword.textProperty().bindBidirectional(newKorisnik.jmbgProperty() );
+                //  dpBirthday.textProperty().unbindBidirectional(oldKorisnik.passwordProperty() );
+            }
+        });
+            fldName.textProperty().addListener((obs, oldIme, newIme) -> {
+                if (!newIme.isEmpty()) {
+                    fldName.getStyleClass().removeAll("poljeNijeIspravno");
+                    fldName.getStyleClass().add("poljeIspravno");
+                } else {
+                    fldName.getStyleClass().removeAll("poljeIspravno");
+                    fldName.getStyleClass().add("poljeNijeIspravno");
+                }
+            });
+
+            fldSurname.textProperty().addListener((obs, oldIme, newIme) -> {
+                if (!newIme.isEmpty()) {
+                    fldSurname.getStyleClass().removeAll("poljeNijeIspravno");
+                    fldSurname.getStyleClass().add("poljeIspravno");
+                } else {
+                    fldSurname.getStyleClass().removeAll("poljeIspravno");
+                    fldSurname.getStyleClass().add("poljeNijeIspravno");
+                }
+            });
+
+        fldEmail.textProperty().addListener((obs, oldIme, newIme) -> {
+            if (!newIme.isEmpty()) {
+                if(newIme.contains(".com") && newIme.contains("@") && newIme.length()>=3){
+                    if(Character.isLetter(newIme.charAt(0)) && Character.isLetter(newIme.charAt(newIme.length()-1))) {
+                        fldEmail.getStyleClass().removeAll("poljeNijeIspravno");
+                        fldEmail.getStyleClass().add("poljeIspravno");
+                    }
+                    else {
+                        fldEmail.getStyleClass().removeAll("poljeIspravno");
+                        fldEmail.getStyleClass().add("poljeNijeIspravno");
+                    }
+                }
+                else {
+                    fldEmail.getStyleClass().removeAll("poljeIspravno");
+                    fldEmail.getStyleClass().add("poljeNijeIspravno");
+                }
+            } else {
+                fldEmail.getStyleClass().removeAll("poljeIspravno");
+                fldEmail.getStyleClass().add("poljeNijeIspravno");
+            }
+        });
 
 
+            fldUsername.textProperty().addListener((obs, oldIme, newIme) -> {
+                if (!newIme.isEmpty()) {
+                    fldUsername.getStyleClass().removeAll("poljeNijeIspravno");
+                    fldUsername.getStyleClass().add("poljeIspravno");
+                } else {
+                    fldUsername.getStyleClass().removeAll("poljeIspravno");
+                    fldUsername.getStyleClass().add("poljeNijeIspravno");
+                }
+            });
+
+        fldJmbg.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observableValue, String o, String n) {
+                if (isJmbgValid(n)) {
+                    fldJmbg.getStyleClass().removeAll("poljeNijeIspravno");
+                    fldJmbg.getStyleClass().add("poljeIspravno");
+                } else {
+                    fldJmbg.getStyleClass().removeAll("poljeIspravno");
+                    fldJmbg.getStyleClass().add("poljeNijeIspravno");
+                }
+            }
+        });
+        dpBirthday.valueProperty().addListener(new ChangeListener<LocalDate>() {
+            @Override
+            public void changed(ObservableValue<? extends LocalDate> observableValue, LocalDate o, LocalDate n) {
+                if (isDateValid(n))
+                {
+                    dpBirthday.getStyleClass().removeAll("poljeNijeIspravno");
+                    dpBirthday.getStyleClass().add("poljeIspravno");
+                }
+                else {
+                    dpBirthday.getStyleClass().removeAll("poljeIspravno");
+                    dpBirthday.getStyleClass().add("poljeNijeIspravno");
+                }
+            }
+        });
+
+
+        fldPassword.textProperty().addListener((obs, oldIme, newIme) -> {
+            if (!newIme.isEmpty() || newIme.length()<3) {
+                fldPassword.getStyleClass().removeAll("poljeNijeIspravno");
+                fldPassword.getStyleClass().add("poljeIspravno");
+            } else {
+                fldPassword.getStyleClass().removeAll("poljeIspravno");
+                fldPassword.getStyleClass().add("poljeNijeIspravno");
+            }
         });
     }
+
+    private boolean isDateValid(LocalDate s) {
+        if (s == null) return false;
+        LocalDate trenutni = LocalDate.now();
+        if (s.isAfter(trenutni)) return false;
+        return true;
+    }
+
     public void btnSubjectAdd(ActionEvent actionEvent) {
     }
 
--- src/ba/unsa/etf/rs/User.java
+++ src/ba/unsa/etf/rs/User.java
@@ -1,5 +1,6 @@
 package ba.unsa.etf.rs;
 
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
 
 import java.sql.Date;
@@ -7,11 +8,11 @@ import java.time.LocalDate;
 
 public class User {
     private SimpleStringProperty  name;
-    private String surname;
-    private String email;
-    private String jmbg;
-    private String username;
-    private Date dateOfBirth;
+    private SimpleStringProperty surname;
+    private SimpleStringProperty email;
+    private SimpleStringProperty jmbg;
+    private SimpleStringProperty username;
+    private SimpleObjectProperty<Date> dateOfBirth;
 
     @Override
     public String toString() {
@@ -19,12 +20,12 @@ public class User {
     }
 
     public User(String name, String surname, String email, String jmbg, String username, Date dateOfBirth) {
-        this.name = new SimpleStringProperty(name);;
-        this.surname = surname;
-        this.email = email;
-        this.jmbg = jmbg;
-        this.username = username;
-        this.dateOfBirth = dateOfBirth;
+        this.name = new SimpleStringProperty(name);
+        this.surname = new SimpleStringProperty(surname);
+        this.email =new SimpleStringProperty(email);
+        this.jmbg = new SimpleStringProperty(jmbg);
+        this.username = new SimpleStringProperty(username);
+        this.dateOfBirth = new SimpleObjectProperty<Date>(dateOfBirth);
     }
 
     public String getName() {
@@ -40,42 +41,62 @@ public class User {
     }
 
     public String getSurname() {
+        return surname.get();
+    }
+
+    public SimpleStringProperty surnameProperty() {
         return surname;
     }
 
     public void setSurname(String surname) {
-        this.surname = surname;
+        this.surname.set(surname);
     }
 
     public String getEmail() {
+        return email.get();
+    }
+
+    public SimpleStringProperty emailProperty() {
         return email;
     }
 
     public void setEmail(String email) {
-        this.email = email;
+        this.email.set(email);
     }
 
     public String getJmbg() {
+        return jmbg.get();
+    }
+
+    public SimpleStringProperty jmbgProperty() {
         return jmbg;
     }
 
     public void setJmbg(String jmbg) {
-        this.jmbg = jmbg;
+        this.jmbg.set(jmbg);
     }
 
     public String getUsername() {
+        return username.get();
+    }
+
+    public SimpleStringProperty usernameProperty() {
         return username;
     }
 
     public void setUsername(String username) {
-        this.username = username;
+        this.username.set(username);
     }
 
     public Date getDateOfBirth() {
+        return dateOfBirth.get();
+    }
+
+    public SimpleObjectProperty<Date> dateOfBirthProperty() {
         return dateOfBirth;
     }
 
     public void setDateOfBirth(Date dateOfBirth) {
-        this.dateOfBirth = dateOfBirth;
+        this.dateOfBirth.set(dateOfBirth);
     }
 }
