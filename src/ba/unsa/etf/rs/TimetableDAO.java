851da6b80224e4b7d4977da459cee129114b3066
Emir Kurtovic <ekurtovic3@etf.unsa.ba>
2020-09-01 00:18:34 +0200

    TimetableDAO-->SubjectDAO

--- src/ba/unsa/etf/rs/TimetableDAO.java
+++ src/ba/unsa/etf/rs/TimetableDAO.java
@@ -22,6 +22,117 @@ public class TimetableDAO{
     private PreparedStatement findClassroomByIDQuery,findClassByIDQuery;
 
 
+
+    //PredmetDAO
+
+    public void defaultSubject() {
+        addSubject(new Subject("Fizika"));
+        addSubject(new Subject("Matematika"));
+        addSubject(new Subject("asd"));
+
+    }
+    public void TESTdeleteSubject() {
+        deleteSubject("Fizika");
+        deleteSubject("Matematika");
+        deleteSubject("asd");
+    }
+    public Subject findSubject(String subjectName) {
+        Subject result = null;
+        try {
+            findSubjectQuery.setString(1,subjectName);
+            ResultSet resultSet = findSubjectQuery.executeQuery();
+            while (resultSet.next())
+                result = new Subject(resultSet.getString(2));
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+    public int findSubjectID(String subjectName) {
+        int result=0;
+        try {
+            findSubjectQuery.setString(1,subjectName);
+            ResultSet resultSet = findSubjectQuery.executeQuery();
+            while (resultSet.next())
+                result = resultSet.getInt(1);
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public int findMaxIDSubject() {
+        int result = 0;
+        try {
+            ResultSet resultSet = findMaxIDSubject.executeQuery();
+            while (resultSet.next())
+                result = resultSet.getInt(1);
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public boolean addSubject(Subject sub) {
+        try  {
+            //Subject s = findSubject(sub.getName());
+            ID = findMaxIDSubject() + 1;
+            int subjectID = ID;
+            //   sub.setId(subjectID);
+            addSubjectQuery.setInt(1,subjectID);
+            addSubjectQuery.setString(2, sub.getName());
+            addSubjectQuery.executeUpdate();
+            System.out.println(sub.getName() + " " + ID);
+            return true;
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public ObservableList<Subject> getAllSubjects() {
+        ArrayList<Subject> result = new ArrayList<>();
+        try {
+            ResultSet resultSet = selectSubjects.executeQuery();
+            while (resultSet.next())
+                result.add(new Subject(resultSet.getString(2)));
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return FXCollections.observableArrayList(result);
+    }
+    public void deleteSubject(String s) {
+        try {
+            Subject ps= findSubject(s);
+            deleteSubject.setString(1,ps.getName());
+            deleteSubject.executeUpdate();
+
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    public Subject findSubjectByID(int id) {
+        Subject result = null;
+        try {
+            findClassByIDQuery.setInt(1,id);
+            ResultSet resultSet = findClassByIDQuery.executeQuery();
+            while (resultSet.next())
+                result = new Subject(resultSet.getString(2));
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+
+    ///// Ostalo
         public static void deleteInstance(){
             try {
                 conn.close();
